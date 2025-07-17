9ebbe4880a6e154f4cf6d6f5ad0d04b88fe3c8f1
code_blooded <carigokaren14@gmail.com>
2025-07-18 03:52:53 +0800

    created home page and routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  // Future routes will be added here
+  // { path: 'survey', component: SurveyComponent },
+  // { path: 'movies', component: MoviesComponent },
+  { path: '**', redirectTo: '' } // Wildcard route for 404 pages
+];
