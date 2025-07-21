e0b8817d3e98ce110bd67e82f494df33e62e3942
DaveDominicLactuan <davedominic.lactuan.22@usjr.edu.ph>
2025-07-21 11:35:51 +0800

    commit

---
+++ frontend/src/app/pages/movies-page/movies-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-page',
+  imports: [],
+  templateUrl: './movies-page.html',
+  styleUrl: './movies-page.scss'
+})
+export class MoviesPage {
+
+}
